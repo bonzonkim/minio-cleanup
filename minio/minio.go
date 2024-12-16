d30d5e0736ca0f3c7caa0c1735ed3301eade5086
Bumgu Kang <flrhyme9@gmail.com>
2024-12-16 17:56:40 +0900

    feat: updated minio sdk to v7

--- minio/minio.go
+++ minio/minio.go
@@ -1,15 +1,22 @@
 package minioUtils
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
-	"github.com/minio/minio-go"
+	//"github.com/minio/minio-go"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 func ConnectMinio(endpoint string, accessKeyID string, secretAccessKey string, useSSL bool) *minio.Client {
-	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	//minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds: credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
 	if err != nil {
 		log.Fatalf("Failed to connect to MinIO: %v\n", err)
 		return nil
@@ -20,19 +27,22 @@ func ConnectMinio(endpoint string, accessKeyID string, secretAccessKey string, u
 
 
 
-func removeObject(bucketName string, objectName string, minioClient *minio.Client) (bool, error) {
-	if err := minioClient.RemoveObject(bucketName, objectName); err != nil {
+func removeObject(ctx context.Context, bucketName string, objectName string, minioClient *minio.Client) (bool, error) {
+	if err := minioClient.RemoveObject(ctx, bucketName, objectName, minio.RemoveObjectOptions{}); err != nil {
 		return false, err
 	}
 	return true, nil
 }
 
-func listObjects(bucketName string, recursive bool, minioClient *minio.Client) <-chan minio.ObjectInfo {
+func listObjects(ctx context.Context, bucketName string, recursive bool, minioClient *minio.Client) <-chan minio.ObjectInfo {
 	doneCh := make(chan minio.ObjectInfo)
 
 	go func() {
 		defer close(doneCh)
-		objectCh := minioClient.ListObjects(bucketName, "", recursive, nil)
+		objectCh := minioClient.ListObjects(ctx, bucketName, minio.ListObjectsOptions{
+			Prefix: "",
+			Recursive: true,
+		})
 		for object := range objectCh {
 			if object.Err != nil {
 				log.Printf("Error occurred for %s: %v\n:", object.Key, object.Err)
@@ -45,8 +55,10 @@ func listObjects(bucketName string, recursive bool, minioClient *minio.Client) <
 }
 
 func RemoveObjectsBeforeWeek(bucketName string, recursive bool, minioClient *minio.Client) {
-	daysAgo := time.Now().AddDate(0, 0, -5)
-	objectCh := listObjects(bucketName, recursive, minioClient)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	daysAgoFromNow := time.Now().AddDate(0, 0, -5)
+	objectCh := listObjects(ctx, bucketName, recursive, minioClient)
 
 	for object := range objectCh {
 		if object.Key == "loki_cluster_seed.json" {
@@ -54,11 +66,11 @@ func RemoveObjectsBeforeWeek(bucketName string, recursive bool, minioClient *min
 			continue
 		}
 
-		if !object.LastModified.Before(daysAgo) {
+		if !object.LastModified.Before(daysAgoFromNow) {
 			continue
 		}
 
-		ok, err := removeObject(bucketName, object.Key, minioClient)
+		ok, err := removeObject(ctx, bucketName, object.Key, minioClient)
 		if err != nil {
 			log.Printf("Error occcurred while deleting %s : %v\n", object.Key, err)
 			continue
